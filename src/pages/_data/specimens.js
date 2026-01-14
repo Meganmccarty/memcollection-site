ce6cc437a6aa6bda0aac58e2ba1111179c9ac658
Megan McCarty <megan_mccarty@hotmail.com>
2026-01-14 18:51:58 -0500

    Set up GHA to deploy to NFS, remove vercel

--- src/pages/_data/specimens.js
+++ src/pages/_data/specimens.js
@@ -5,14 +5,25 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV}` });
 
 export default async function getSpecimens() {
+    let allItems = [];
+    let currentOffset = 0; // what index the results start at (0 = 1)
     // Need to set a limit when this fetch is executed in the CI; otherwise, it fails
-    const limit = process.env.CI ? 20 : 10000;
-    const url = `${process.env.API_BASE_URL}/specimen-records/?limit=${limit}`;
+    const limit = process.env.NODE_ENV === 'ci' ? 20 : 500; // the max amount of results returned per query
 
-    const data = await EleventyFetch(url, {
-        duration: '1d',
-        type: 'json',
-    });
+    // Setting the for loop to run 15 times (15 loops x 500 result limit = 7,500 specimens)
+    for (let i = currentOffset; i < 15; i += 1) {
+        const url = `${process.env.API_BASE_URL}/specimen-records/?offset=${currentOffset}&limit=${limit}`;
 
-    return data.items;
+        /* eslint-disable no-await-in-loop */
+        const data = await EleventyFetch(url, {
+            duration: '1d',
+            type: 'json',
+        });
+
+        allItems = allItems.concat(data.items);
+        currentOffset += limit;
+        setTimeout(() => {}, 250);
+    }
+
+    return allItems;
 }
