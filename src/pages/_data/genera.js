761d6f86289dbe0f532302d76dbd9c9c83ef85e7
Megan McCarty <megan_mccarty@hotmail.com>
2026-07-27 21:21:38 -0400

    Add missing tribe and genus fixtures, update test data

--- src/pages/_data/genera.js
+++ src/pages/_data/genera.js
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import EleventyFetch from '@11ty/eleventy-fetch';
 import { config } from 'dotenv';
-// import * as testGenera from './fixtures/genera.json' with { type: 'json' };
+import * as testGenera from './fixtures/genera.json' with { type: 'json' };
 
 config({ path: `.env.${process.env.NODE_ENV}` });
 
 export default async function getGenera() {
-    // if (process.env.ENV === 'ci') {
-    //     return testGenera.default.items;
-    // }
+    if (process.env.ENV === 'ci') {
+        return testGenera.default.items;
+    }
 
     const url = `${process.env.API_BASE_URL}/nested-genera/?limit=4000`;
 
--- src/pages/_data/tribes.js
+++ src/pages/_data/tribes.js
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import EleventyFetch from '@11ty/eleventy-fetch';
 import { config } from 'dotenv';
-// import * as testTribes from './fixtures/tribes.json' with { type: 'json' };
+import * as testTribes from './fixtures/tribes.json' with { type: 'json' };
 
 config({ path: `.env.${process.env.NODE_ENV}` });
 
 export default async function getTribes() {
-    // if (process.env.ENV === 'ci') {
-    //     return testTribes.default.items;
-    // }
+    if (process.env.ENV === 'ci') {
+        return testTribes.default.items;
+    }
 
     const url = `${process.env.API_BASE_URL}/nested-tribes/?limit=4000`;
 
